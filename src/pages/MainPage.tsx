dd82345166aa652551eb3ac5d728b0fe0ef15a2f
JellyZergling <77082540+JellyZergling@users.noreply.github.com>
2024-01-08 15:07:19 +0900

    Basic Setting

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,13 @@
+import Layout from "../components/Layout/Layout"
+
+function MainPage(){
+  return(
+    <Layout>
+      <div>test
+          <h1>hi</h1>
+      </div>
+    </Layout>
+  )
+}
+
+export default MainPage
